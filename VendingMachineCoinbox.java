19c80d39dbd32959f6471db96219d1f4df851525
Colin Howard <colin.ad.astra@gmail.com>
2023-09-21 19:21:23 -0400

    Added inventory and test program

---
+++ VendingMachineCoinbox.java
@@ -0,0 +1,16 @@
+public class VendingMachineCoinbox {
+    public final static char[] acceptable_coin_chars = {'p','n','d','q','h','g'};
+    public final static int[] acceptable_coin_vals = {1,5,10,25,50,100};
+
+    public static void main(String[] args) {
+        
+    }
+
+    public void acceptCoin(char coin_type) {
+
+    }
+
+    public void dispenseChange() {
+
+    }
+}
---
+++ VendingMachineInventory.java
@@ -0,0 +1,23 @@
+public class VendingMachineInventory {
+    private String[] itemNames;
+    private int[][] numCosts;
+
+    public VendingMachineInventory() {
+        itemNames = new String[] {"Doritos", "Oreos", "Snickers", "Water", "Dr. Pepper"};
+        numCosts = new int[][] {{5,5,5,5,5},{1,2,1,2,1}};
+    }
+
+    public String toString() {
+        String summary = "Item Name\tQuantity\tCost\n";
+        String tabs = "";
+        for (int i = 0; i < itemNames.length; i++) {
+            if (itemNames[i].length()<8) {
+                tabs = "\t\t";
+            } else {
+                tabs = "\t";
+            }
+            summary += itemNames[i] + tabs + numCosts[0][i] + "\t\t" + numCosts[1][i] + "\n";
+        }
+        return summary;
+    }
+}
