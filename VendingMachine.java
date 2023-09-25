517e605367e19748809703352b7e2d14d3ac9206
mrlmoffitt <lmoffitt@schools.nyc.gov>
2023-09-25 15:46:41 -0400

    Testing

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,10 @@
+/**
+This is the vending machine for ASYNC 709
+@author luke 
+@author colin
+*/
+
+
 public class VendingMachine {
     public static void main(String[] args) {
         VendingMachineInventory inventory = new VendingMachineInventory();
